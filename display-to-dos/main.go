27ab01642f89deb1af605edd6b7ea4eb22ee98b7
Owen Corrigan (BJSS, VTM) <122352145+owen-corrigan-bjss@users.noreply.github.com>
2024-08-19 09:53:56 +0100

    first pass at task 13

--- display-to-dos/main.go
+++ display-to-dos/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -27,20 +28,47 @@ func PrintToDosList(toDos ...toDos.ToDo) string {
 	return toDoStr
 }
 
+func CreateNewJsonFile() {
+	toDosForJson := []toDos.ToDo{{Description: "this is a new JSON file", Complete: false}}
+
+	for i := 1; i < 10; i++ {
+		newToDo := toDos.ToDo{Description: fmt.Sprintf("this is new task %d", i+1), Complete: false}
+		toDosForJson = append(toDosForJson, newToDo)
+	}
+
+	file, err := os.Create("/Users/owen.corrigan/projects/go-to-do/display-to-dos/newToDos.json")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	toDoList := toDos.ToDoListForConvert{}
+
+	toDoList.ToDos = toDosForJson[:]
+
+	jsonFormattedToDos, err := json.Marshal(toDoList)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = file.Write(jsonFormattedToDos)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	breakLoop := false
 
 	for !breakLoop {
-		toDoJson, err := os.Open("/Users/owen.corrigan/projects/go-to-do/toDos.json")
-		if err != nil {
-			log.Fatal(err)
-		}
 
-		defer toDoJson.Close()
-
-		fmt.Printf("what would you like to do:\n1: print to do from JSON list\n2: print toDos from an array\n0: exit\n")
+		fmt.Printf("what would you like to do:\n1: print to do from JSON list\n2: print toDos from an array\n3: create a new JSON file and print it\n0: exit\n")
 
 		scanner.Scan()
 		input := scanner.Text()
@@ -48,11 +76,25 @@ func main() {
 		if input == "0" {
 			breakLoop = true
 		} else if input == "1" {
+			toDoJson, err := os.Open("/Users/owen.corrigan/projects/go-to-do/toDos.json")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			defer toDoJson.Close()
 			decodedJson := helpers.DecodeJson(toDoJson)
 			PrintJsonToDos(decodedJson...)
 		} else if input == "2" {
-
 			PrintToDosList(toDos.ToDoList...)
+		} else if input == "3" {
+			newToDoJson, err := os.Open("/Users/owen.corrigan/projects/go-to-do/toDos.json")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer newToDoJson.Close()
+			CreateNewJsonFile()
+			decodedJson := helpers.DecodeJson(newToDoJson)
+			PrintJsonToDos(decodedJson...)
 		}
 	}
 }
