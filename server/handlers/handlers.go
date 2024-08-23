78997d4a7af24d16d3b5a760d9831d894296eb5f
Owen Corrigan (BJSS, VTM) <122352145+owen-corrigan-bjss@users.noreply.github.com>
2024-08-23 14:48:02 +0100

    adds put and delete endpoints

--- server/handlers/handlers.go
+++ server/handlers/handlers.go
@@ -14,7 +14,7 @@ type ToDoReq struct {
 	Description string `json:"description"`
 }
 
-type ToDoCreatedResponse struct {
+type ToDoResponse struct {
 	Id          string
 	Description string
 	Status      bool
@@ -38,7 +38,7 @@ func HandleCreateNewToDo(res http.ResponseWriter, req *http.Request) {
 
 	newToDo := inMemoryToDoList.GetSingleToDo(newItemKey)
 
-	resBody := ToDoCreatedResponse{newItemKey, newToDo.Description, newToDo.Completed}
+	resBody := ToDoResponse{newItemKey, newToDo.Description, newToDo.Completed}
 
 	res.Header().Set("Content-Type", "application/json")
 	res.WriteHeader(201)
@@ -53,7 +53,36 @@ func HandleListToDos(res http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(res).Encode(toDos)
 }
 
+func HandleUpdateToDo(res http.ResponseWriter, req *http.Request) {
+	query := req.URL.Query()
+	id := query.Get("id")
+
+	inMemoryToDoList.UpdateToDoItemStatus(id)
+	updatedToDo := inMemoryToDoList.GetSingleToDo(id)
+	fmt.Println(updatedToDo)
+	responseBody := ToDoResponse{id, updatedToDo.Description, updatedToDo.Completed}
+
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(200)
+	json.NewEncoder(res).Encode(responseBody)
+}
+
+func HandleDeleteToDo(res http.ResponseWriter, req *http.Request) {
+	query := req.URL.Query()
+	id := query.Get("id")
+
+	inMemoryToDoList.DeleteToDoItem(id)
+
+	responseBody := fmt.Sprintf("todo: %s deleted", id)
+
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(200)
+	json.NewEncoder(res).Encode(responseBody)
+}
+
 func Handlers() {
-	http.HandleFunc("POST /create", HandleCreateNewToDo)
+	http.HandleFunc("POST /todos", HandleCreateNewToDo)
 	http.HandleFunc("GET /todos", HandleListToDos)
+	http.HandleFunc("PUT /todos", HandleUpdateToDo)
+	http.HandleFunc("DELETE /todos", HandleDeleteToDo)
 }
--- server/main.go
+++ server/main.go
@@ -1,33 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"sync"
 	handlers "to-do-app/server/handlers"
 )
-
-type ToDo struct {
-	description string
-	completed   bool
-	lock        sync.Mutex
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		handleGetHello(w, r)
-	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
-}
-
-func handleGetHello(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode("hi")
-}
 
 func main() {
 	handlers.Handlers()
